018a3b074c4e76fa62b36595f85d5608a4f2020b
mateuscelio <mateuscelio10@gmail.com>
2022-02-24 19:21:02 -0300

    Removes lambdas

--- app/services/purchase_processes/purchase_creator.rb
+++ app/services/purchase_processes/purchase_creator.rb
@@ -1,14 +1,14 @@
 module PurchaseProcesses
   class PurchaseCreator
     PURCHASE_PROCESSES = {
-      'paypal': ->(cart_id, address, user) { PaypalPurchaseProcess.new(cart_id, address, user) },
-      'stripe': ->(cart_id, address, user) { StripePurchaseProcess.new(cart_id, address, user) }
+      'paypal': PaypalPurchaseProcess,
+      'stripe': StripePurchaseProcess
     }
     def self.call(gateway, cart_id, address, user)
       process = PURCHASE_PROCESSES[gateway.to_sym]
       return ServiceResult.error('Gateway not supported!') unless process
 
-      process.call(cart_id, address, user).create_purchase_order
+      process.new(cart_id, address, user).create_purchase_order
     end
   end
 end
