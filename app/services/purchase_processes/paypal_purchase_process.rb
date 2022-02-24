e51fd27b7b8a23c01a72e80af38de0ca83137dd3
mateuscelio <mateuscelio10@gmail.com>
2022-02-24 19:00:35 -0300

    Moves repeated behavior of purchase process to parent class

--- app/services/purchase_processes/paypal_purchase_process.rb
+++ app/services/purchase_processes/paypal_purchase_process.rb
@@ -1,22 +1,11 @@
 module PurchaseProcesses
   class PaypalPurchaseProcess < BasePurchaseProcess
-    attr_reader :cart_id, :address, :user_params
-
     def initialize(cart_id, address, user_params)
-      @cart_id = cart_id
-      @address = address
-      @user_params = user_params
+      super
     end
 
     def create_purchase_order
-      cart = Cart.find_by(id: cart_id)
-      return ServiceResult.error('Cart not found!') unless cart
-
-      user_res = CartUserCreator.call(cart, user_params)
-
-      return user_res unless user_res.success?
-
-      OrderCreator.call(user_res.value, cart, address)
+      super
     end
   end
 end
