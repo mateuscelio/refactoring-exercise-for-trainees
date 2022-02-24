516d32e90fc4683a0df6e8342d14045944e87b65
mateuscelio <mateuscelio10@gmail.com>
2022-02-24 14:06:59 -0300

    Abstract order creation

---
+++ spec/services/order_creator_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+RSpec.describe OrderCreator do
+  let(:address_params) do
+    {
+      user: user,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      address_1: 'address_1',
+      address_2: 'address_2',
+      city: 'city',
+      state: 'state',
+      country: 'country',
+      zip: 'zip'
+    }
+  end
+  let(:cart) { Cart.create}
+
+  context 'with valid user' do
+    let(:user) { User.create(email: 'user@spec.io', first_name: 'John', last_name: 'Doe') }
+
+    it 'creates valid order' do
+      expect(OrderCreator.call(user, cart, address_params).valid?).to eq(true)
+    end
+  end
+  context 'with invalid user' do
+    let(:user) { User.create }
+    it 'does not create valid order' do
+      expect(OrderCreator.call(user, cart, address_params).valid?).to eq(false)
+    end
+  end
+end
